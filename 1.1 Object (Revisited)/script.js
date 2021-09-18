24d5f31e8cf3dac9bb6239c3b5db9190a929c38a
muhammad-dicky <dickyzs155@gmail.com>
2021-09-18 17:59:47 +0800

    first commit

---
+++ 1.1 Object (Revisited)/script.js
@@ -0,0 +1,27 @@
+// Cara untuk membuat Object pada javascript
+// 1. Object Literal
+let mahasiswa1 = {
+    nama: 'Sandhika',
+    energi: 10,
+    makan: function(porsi){
+        this.energi = this.energi + porsi;
+        console.log(`Hallo ${this.nama}, selamat makan!`)
+    
+    }
+}
+let mahasiswa2 = {
+    nama: 'Doddy',
+    energi: 10,
+    makan: function(porsi){
+        this.energi = this.energi + porsi;
+        console.log(`Hallo ${this.nama}, selamat makan!`)
+    
+    }
+}
+
+
+
+// 2. Function Declaration
+// 3. Constructor Function
+// 4. Object.Create
+
