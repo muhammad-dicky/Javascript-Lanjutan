fdce3fbc758f1131f3f87b778a35c9db18547919
muhammad-dicky <dickyzs155@gmail.com>
2021-09-22 15:52:58 +0800

    updateee

--- 2.1 Execution Context, Hoisting & Scope/script.js
+++ 2.1 Execution Context, Hoisting & Scope/script.js
@@ -10,14 +10,54 @@
 
 
 
-// belum selesai 10:35
 
-var nama = 'Diki';
-var umur = 21;
+// var nama = 'Diki';
+// var umur = 21;
 
-console.log(sayHello());
+// console.log(sayHello());
 
 
-function sayHello() {
-    return `hallo, nama saya ${nama}, umur ${umur} tahun`;
+// function sayHello() {
+//     return `hallo, nama saya ${nama}, umur ${umur} tahun`;
+// }
+
+// function membuat Local Execution Context
+// yang didalamnya terdapat creation dan execution phase :
+// window
+// arguments (yang bisa berisi parameter dari object, "function sayHello(parameter)" )
+// local hoisting
+
+
+
+// var nama = "Muhammad Dicky";
+// var username = '@dickyn';
+
+// function cetakURL(username) {
+//     var instagramURL = 'http://instagram.com/';
+//     return instagramURL + username;
+
+// };
+
+// console.log(cetakURL(username));
+
+
+// contoh stack hoisting
+
+var nama = "Dicky";
+
+function a() {
+    console.log("hallo ini a");
+
+    function b() {
+        console.log("hallo ini b");
+
+        function c() {
+            console.log("hallo ini c");
+        }
+        c();
+    }
+    b();
 }
+a();
+
+console.log(a());
